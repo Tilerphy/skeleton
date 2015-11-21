5791e1c99500805f4fd0eb1fbf407d3cfb4e0143
pesetskyps <pavel_pesetskiy@epam.com>
2015-11-21 08:36:47 +0300

    done

--- src/main/java/com/jiradev/jira/plugins/panels/issue/UserRoleIssueAction.java
+++ src/main/java/com/jiradev/jira/plugins/panels/issue/UserRoleIssueAction.java
@@ -8,10 +8,26 @@ import com.atlassian.jira.project.Project;
 import com.atlassian.jira.security.roles.ProjectRole;
 import com.atlassian.jira.security.roles.ProjectRoleActors;
 import com.atlassian.jira.security.roles.ProjectRoleManager;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Map;
-import java.util.TreeMap;
+import com.plex.smash.client.SmashClient;
+import com.plex.smash.client.SmashConfiguration;
+import com.plex.smash.client.jobs.JobConfigurationQuery;
+import com.plex.smash.client.jobs.JobConfigurationService;
+import com.plex.smash.client.jobs.JobConfigurationSimple;
+import com.plex.smash.client.jobs.JobConfigurationSimpleList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.*;
 
 /**
  * Created by Pavel_Pesetskiy on 11/20/2015.
@@ -20,6 +36,16 @@ public class UserRoleIssueAction extends AbstractIssueAction {
     private ProjectRoleManager projectRoleManager = ComponentAccessor.getComponent(ProjectRoleManager.class);
     private TreeMap people = new TreeMap();
     private Project project;
+    private Map<String, String> JobNameUpdateDate;
+    private static final Logger log = LoggerFactory.getLogger(UserRoleIssueTabPanel.class);
+    private SmashClient smashClient;
+    private JobConfigurationSimpleList jobbbs;
+    static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
 
     public UserRoleIssueAction(IssueTabPanelModuleDescriptor issueTabPanelModuleDescriptor, Project project){
         super(issueTabPanelModuleDescriptor);
@@ -30,15 +56,61 @@ public class UserRoleIssueAction extends AbstractIssueAction {
         return null;
     }
 
-    public void populateVelocityParams(Map params){
-        //Get all the project roles
-        Collection<ProjectRole> projectRoles = projectRoleManager.getProjectRoles();
-        //Iterate through each role and get the users associated with the role
-        for (ProjectRole projectRole : projectRoles){
-            ProjectRoleActors roleActors = projectRoleManager.getProjectRoleActors(projectRole, project);
-            people.put(projectRole.getName(),roleActors.getUsers());
+
+    public static String getBambooConnectorPrivateKey() {
+        String filename = "d:/downloads/1448079544013.pem";
+
+
+        String str = "";
+        try {
+            str = readFile(filename, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        params.put("people",people);
-        params.put("avatarService", ComponentAccessor.getAvatarService());
+        return str;
+    }
+
+    public void populateVelocityParams(Map params){
+        SmashConfiguration smashConfiguration = new SmashConfiguration();
+        smashConfiguration.setEndpoint("https://code.plex.com");
+        String key = getBambooConnectorPrivateKey();
+        smashConfiguration.setPrivateKey(key);
+        smashClient = SmashClient.getClient(smashConfiguration);
+        JobConfigurationService jobs = smashClient.getJobs();
+        JobConfigurationQuery query = new JobConfigurationQuery();
+        query.setCreatedBy(4);
+
+
+        try {
+            jobbbs = jobs.query(query);
+            JobNameUpdateDate = new HashMap<String, String>();
+            for(Iterator<JobConfigurationSimple> i = jobbbs.iterator(); i.hasNext(); ) {
+                JobConfigurationSimple item = i.next();
+                String date = "";
+                if(item.getUpdatedDate() != null){
+                    date = item.getUpdatedDate().toString();
+                }
+                JobNameUpdateDate.put(item.getName(), date);
+
+                System.out.println(item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        log.info("Smash client initialized.");
+        params.put("jobs",JobNameUpdateDate);
+        //params.put("avatarService", ComponentAccessor.getAvatarService());
     }
 }
--- src/main/java/com/jiradev/jira/plugins/panels/issue/UserRoleIssueTabPanel.java
+++ src/main/java/com/jiradev/jira/plugins/panels/issue/UserRoleIssueTabPanel.java
@@ -2,6 +2,13 @@ package com.jiradev.jira.plugins.panels.issue;
 
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.util.EasyList;
+import com.plex.smash.client.SmashClient;
+import com.plex.smash.client.SmashConfiguration;
+import com.plex.smash.client.jobs.JobConfigurationQuery;
+import com.plex.smash.client.jobs.JobConfigurationService;
+import com.plex.smash.client.jobs.JobConfigurationSimple;
+import com.plex.smash.client.jobs.JobConfigurationSimpleList;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,28 +18,46 @@ import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.crowd.embedded.api.User;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.*;
 //import com.plex.smash.client.SmashClient;
 
 public class UserRoleIssueTabPanel extends AbstractIssueTabPanel
 {
     private static final Logger log = LoggerFactory.getLogger(UserRoleIssueTabPanel.class);
+    private SmashClient smashClient;
+    private JobConfigurationSimpleList jobbbs;
+    static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
 
     public static String getBambooConnectorPrivateKey() {
-        String filename = "d:/1448058129759.pem";
-        String key = null;
+        String filename = "d:/downloads/1448079544013.pem";
+
+
+        String str = "";
         try {
-            key = IOUtils.toString(UserRoleIssueTabPanel.class.getClassLoader().getResourceAsStream(filename), "UTF8");
+            str = readFile(filename, StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return key;
+        return str;
     }
 
     public List getActions(Issue issue, ApplicationUser remoteUser) {
-
+        log.debug(issue.getId().toString());
         return EasyList.build(new UserRoleIssueAction(super.descriptor, issue.getProjectObject()));
     }
 
